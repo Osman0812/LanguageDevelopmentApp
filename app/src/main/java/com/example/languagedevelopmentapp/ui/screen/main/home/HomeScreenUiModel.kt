dccfbf5412fc60f038b8dd438cb2651696110684
osman <osman.mamedov2000@gmail.com>
2024-03-24 02:40:17 +0300

    fixed gemini bugs, added synonyms part.

--- app/src/main/java/com/example/languagedevelopmentapp/ui/screen/main/home/HomeScreenUiModel.kt
+++ app/src/main/java/com/example/languagedevelopmentapp/ui/screen/main/home/HomeScreenUiModel.kt
@@ -2,5 +2,6 @@ package com.example.languagedevelopmentapp.ui.screen.main.home
 
 data class HomeScreenUiModel(
     val translate: String = "",
-    val otherUsages: String = "",
+    val otherUsagesEnglish: List<String> = emptyList(),
+    val otherUsagesTurkish: List<String> = emptyList(),
 )
