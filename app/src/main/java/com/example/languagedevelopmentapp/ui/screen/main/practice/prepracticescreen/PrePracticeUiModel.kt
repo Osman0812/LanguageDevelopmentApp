06f4681d2d690093cf42607129a9b70326f8e3bd
osman <osman.mamedov2000@gmail.com>
2024-05-11 23:45:47 +0300

    added reading screen translation popup feature

--- app/src/main/java/com/example/languagedevelopmentapp/ui/screen/main/practice/prepracticescreen/PrePracticeUiModel.kt
+++ app/src/main/java/com/example/languagedevelopmentapp/ui/screen/main/practice/prepracticescreen/PrePracticeUiModel.kt
@@ -3,5 +3,7 @@ package com.example.languagedevelopmentapp.ui.screen.main.practice.prepracticesc
 data class PrePracticeUiModel(
     val readingTextWords: String = "",
     val readingTextWordsList: List<String> = emptyList(),
-    val readingText: String = ""
+    val readingText: String = "",
+    val translatedReadingText: String = "",
+    val translatedSelectedText: String = ""
 )
